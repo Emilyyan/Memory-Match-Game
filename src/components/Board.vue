559a5891f3e8d794ceb2e01b5ecae22237ba21a1
Emilyyan <xw324@mail.missouri.edu>
2017-12-10 17:13:13 -0700

    fix merge comflic

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,13 +1,10 @@
 <template>
     <div>
-<<<<<<< HEAD
       <label>{{msg}}</label>
       <br>
       <label>The hard level received by board is {{hard_level}}</label>
       <br>
       <label>{{board_img}}</label>
-=======
->>>>>>> 512124ca7df7e6e2a20a13fcd79d29dee977c04d
       <div class="board-row">
         <template v-if="hard_level === 12">
           <h1>Current level: difficult</h1>
@@ -23,16 +20,12 @@ import square from './square'
 
 export default {
   name: 'Board',
-<<<<<<< HEAD
   props: {
-      hard_level: null,
+      hard_level: Number,
       board_img: Array
 
   },
 
-=======
-  props: {'hard_level':Number},
->>>>>>> 512124ca7df7e6e2a20a13fcd79d29dee977c04d
   data () {
     return {
       parentMsg:["./static/pokemons/004-charmander@3x.png", 
@@ -43,9 +36,8 @@ export default {
   },
   components: {
     square
-<<<<<<< HEAD
-=======
-  },
+  }
+  /*,
   methods: {
     shuffle(a) {
       for (let i = a.length - 1; i > 0; i--) {
@@ -81,9 +73,8 @@ export default {
             break;
        }
        return block;
-    }*/
->>>>>>> 512124ca7df7e6e2a20a13fcd79d29dee977c04d
-  }
+    }
+  }*/
 }
 </script>
 
